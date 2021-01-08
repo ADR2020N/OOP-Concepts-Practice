e317f29bb03bcac86f40b681534cf45d6d6cc094
N.Adrien <71640075+ADR2020N@users.noreply.github.com>
2021-01-08 02:25:55 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,8 @@
+package Abstarct_inteface;
+
+public class Circle implements Shape {    
+	public  Circle() {}	
+	public void draw() {
+		  System.out.println("Cicle Drwan");
+	  }
+}
---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+package Abstarct_inteface;
+
+public class Rectangle implements Shape {   
+	public  Rectangle() {}	
+	public void draw() {
+		  System.out.println("Rectangle Drwan");
+	  }
+}
